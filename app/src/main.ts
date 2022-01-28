81629355dc9d764c1a4250ff47fccf5a4adcc4a8
Vasyl Prokudin <voprokudin@gmail.com>
2022-01-28 16:03:52 +0200

    comment main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,31 +1,31 @@
-import * as core from '@actions/core'
-import sloc from 'node-sloc'
-
-async function run(): Promise<void> {
-  try {
-    // extract inputs
-    const filesAndFoldersToIgnore = JSON.parse(
-      core.getInput('filesAndFoldersToIgnore')
-    )
-    const fileOrFolderToProcess: string = core.getInput('fileOrFolderToProcess')
-
-    // calculate loc stats
-    const stats = await sloc({
-      path: fileOrFolderToProcess,
-      extensions: ['kt'],
-      ignorePaths: filesAndFoldersToIgnore,
-      ignoreDefault: true
-    })
-
-    // set the output of the action
-    core.setOutput('locs', stats?.sloc)
-
-    // debug information is only available when enabling debug logging https://docs.github.com/en/actions/managing-workflow-runs/enabling-debug-logging
-    core.debug(`LOC ${stats?.sloc?.toString() || ''}`)
-
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
-
-run()
+// import * as core from '@actions/core'
+// import sloc from 'node-sloc'
+//
+// async function run(): Promise<void> {
+//   try {
+//     // extract inputs
+//     const filesAndFoldersToIgnore = JSON.parse(
+//       core.getInput('filesAndFoldersToIgnore')
+//     )
+//     const fileOrFolderToProcess: string = core.getInput('fileOrFolderToProcess')
+//
+//     // calculate loc stats
+//     const stats = await sloc({
+//       path: fileOrFolderToProcess,
+//       extensions: ['kt'],
+//       ignorePaths: filesAndFoldersToIgnore,
+//       ignoreDefault: true
+//     })
+//
+//     // set the output of the action
+//     core.setOutput('locs', stats?.sloc)
+//
+//     // debug information is only available when enabling debug logging https://docs.github.com/en/actions/managing-workflow-runs/enabling-debug-logging
+//     core.debug(`LOC ${stats?.sloc?.toString() || ''}`)
+//
+//   } catch (error) {
+//     core.setFailed(error.message)
+//   }
+// }
+//
+// run()
